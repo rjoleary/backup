c7e60a124f628b8a87e41d00da8bff218fe26850
Ryan O'Leary <ryan@ryanoleary.ca>
2017-01-03 19:09:19 -0500

    backup.sh subsumes provision.sh

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@ set -e
 # Ex: ./backup.sh /media/ryan/backup bitbucket github dropbox
 
 BACKUP_PATH=${1-test}
+MY_DIR=$(dirname $0)
 
 # Wait until the backup drive is mounted.
 while [ ! -d "$BACKUP_PATH" ]; do
@@ -14,7 +15,10 @@ done
 for TARGET in "${@:2}"; do
     if [[ "$TARGET" =~ [^a-zA-Z] ]]; then
         echo "\"$TARGET\" does not match ^[a-zA-Z]$"
+    elif [[ ! -x "$MY_DIR/target/$TARGET.sh" ]]; then
+        echo "\"$TARGET\" has to backup script"
     else
-        "$(dirname $0)/target/$TARGET.sh" "$BACKUP_PATH/$TARGET"
+        [ -d "$BACKUP_PATH/$TARGET" ] || mkdir "$BACKUP_PATH/$TARGET"
+        "$MY_DIR/target/$TARGET.sh" "$BACKUP_PATH/$TARGET"
     fi
 done
