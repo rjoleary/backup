26cc6171c036e73062a1f6b65e9c737a2b96ae15
Ryan O'Leary <ryan@ryanoleary.ca>
2017-12-28 02:36:28 -0500

    Remove old bash scripts

--- target/fastmail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-#TODO: incomplete
-
-# This script is inspired by: http://samtuke.com/2014/06/backup-multiple-email-accounts-automatically-on-linux/
-# Make sure to install offlineimap before executing this script:
-#    sudo apt-get install offlineimap
